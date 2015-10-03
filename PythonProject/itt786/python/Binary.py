f35e28b214540a5968b7c9676d82ee3df91f5248
munirahsaad <noormunirah.ms@gmail.com>
2015-10-03 14:01:03 +0800

    update

--- PythonProject/itt786/python/Binary.py
+++ PythonProject/itt786/python/Binary.py
@@ -8,13 +8,13 @@ fw = open('munirah.txt', 'w')
 fw.write('Nama\t:\tNoor Munirah bt Md Saad\nId\t:\t2014828976\nKursus\t:\tCS708\nUniversiti\t:\tUiTM Kampus Shah Alam\n')
 fw.close()
 
-#read the file in ASCII mode
+#read the file in Binary mode
 frb = open('munirah.txt', 'rb')
 txtB = frb.read()
 #print to console
 print(txtB)
 frb.close()
 
-#Write the file in reverse order (ascii)
+#Write the file in reverse order (Binary)
 with open ('munirah.txt', 'rb') as filein, open('munirah_Binary_Reverse.txt', 'wb') as fileout:
     fileout.writelines(reversed(filein.readlines()))
