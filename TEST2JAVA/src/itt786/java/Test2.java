464ef03a963fee797fd2f56d0035a6f87f854d81
munirahsaad <noormunirah.ms@gmail.com>
2015-10-17 07:45:29 +0800

    Mac Adress Module

--- TEST2JAVA/src/itt786/java/Test2.java
+++ TEST2JAVA/src/itt786/java/Test2.java
@@ -1,5 +1,6 @@
 package itt786.java;
 
+import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -13,29 +14,39 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		Test2 t2 = new Test2();
-		t2.getMAC();
+		
+		//Printing Mac Address
+		System.out.println("Mac Address -->\t"+t2.getMAC());
 
 	}
 	
-	public void getMAC(){
+	public String getMAC(){
 		NetworkInterface network;
+		String macAddress = "";
 		try {
-			network = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+			network = NetworkInterface.getByInetAddress(getIPv4Address());
 			byte[] mac = network.getHardwareAddress();
-			System.out.print("Current MAC address : ");
-			
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < mac.length; i++) {
 				sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
 			}
-			System.out.println(sb.toString());			
+			macAddress = sb.toString();		
 			
 		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		
+		return macAddress;
 	}
+	
+	public InetAddress getIPv4Address() throws UnknownHostException{
+		
+		return InetAddress.getLocalHost();
+	}
+	
+	
+
+	
 	
 }
