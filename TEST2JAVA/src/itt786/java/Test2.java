1268b10aea5a2d479ea72aac85fc9880df65fe99
munirahsaad <noormunirah.ms@gmail.com>
2015-10-17 09:13:25 +0800

    Add Thread Module to Company B

--- TEST2JAVA/src/itt786/java/Test2.java
+++ TEST2JAVA/src/itt786/java/Test2.java
@@ -1,10 +1,12 @@
 package itt786.java;
 
-import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Test2 {
 
@@ -17,6 +19,44 @@ public class Test2 {
 		try{
 		//Printing Mac Address
 		System.out.println("Mac Address -->\t\t"+t2.getMAC());
+		
+		
+		
+		//Thread Module
+		// Delay, in milliseconds before we interrupt MessageLoop thread (default one hour).
+        long patience = 1000 * 60 * 60;
+
+        // If command line argument present, gives patience in seconds.
+        if (args.length > 0) {
+            try {
+                patience = Long.parseLong(args[0]) * 1000;
+            } catch (NumberFormatException e) {
+                System.err.println("Argument must be an integer.");
+                System.exit(1);
+            }
+        }
+
+        threadMessage("Starting MessageLoop thread");
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new MessageLoop());
+        t.start();
+
+        threadMessage("Waiting for MessageLoop thread to finish");
+        // loop until MessageLoop thread exits
+        while (t.isAlive()) {
+            threadMessage("Still waiting...");
+            // Wait maximum of 1 second for MessageLoop thread to finish.
+            t.join(1000);
+            if (((System.currentTimeMillis() - startTime) > patience)
+                  && t.isAlive()) {
+                threadMessage("Tired of waiting!");
+                t.interrupt();
+                // Shouldn't be long now -- wait indefinitely
+                t.join();
+            }
+        }
+        threadMessage("Finally!");
+
 				
 		}catch(Exception e){
 			
@@ -49,8 +89,40 @@ public class Test2 {
 		return InetAddress.getLocalHost();
 	}
 	
-
+	
+	private static class MessageLoop implements Runnable {
+	    public void run() {
+	        try {
+	            for (int i = 0;
+	                 i < 10;
+	                 i++) {
+	                // Pause for 4 seconds
+	                Thread.sleep(4000);
+	                
+	                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	                Date date = new Date();
+	                
+	                // Print a message
+	                threadMessage("Current Date/Time : "+dateFormat.format(date));
+	            }
+	        } catch (InterruptedException e) {
+	            threadMessage("Interrupted - I wasn't done!");
+	        }
+	    }
+	}
+	
+	 static void threadMessage(String message) {
+	        String threadName =
+	            Thread.currentThread().getName();
+	        System.out.format("%s: %s%n",
+	                          threadName,
+	                          message);
+	        }
+	
+	 
 }
+
+
 	
 
 	
